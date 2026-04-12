daaf98c832d6700b878715aa59570b49beb51d68
Angel Forneiro <nforneirorosas@gmail.com>
2026-04-12 01:03:48 -0300

    Add pops

--- script.js
+++ script.js
@@ -12,10 +12,36 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(
-  ({ coords }) => {
-    const { latitude } = coords;
-    const { longitude } = coords;
-    console.log(latitude, longitude);
+  position => {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on('click', function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 200,
+            minWidth: 150,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          }),
+        )
+        .setPopupContent('hola')
+        .openPopup();
+    });
   },
   () => {
     // alert("We couldn't get your curret position.");
