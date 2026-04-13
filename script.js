b614e7bfa3908729aa0323255970b75afb7929de
Angel Forneiro <nforneirorosas@gmail.com>
2026-04-13 00:54:51 -0300

    Add form

--- script.js
+++ script.js
@@ -11,39 +11,64 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   position => {
+    // Save coords
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    // Load map
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 200,
-            minWidth: 150,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          }),
-        )
-        .setPopupContent('hola')
-        .openPopup();
+    // Listening to 'click' event on the map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   () => {
     // alert("We couldn't get your curret position.");
   },
 );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Generate popup
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minWidth: 150,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }),
+    )
+    .setPopupContent('hola')
+    .openPopup();
+
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+});
+
+inputType.addEventListener('change', () => {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
